14cdfaa8df28023d04e73f3f4beef022db9e2906
Guy Steele-Perkins <guysteeleperkins@hotmail.com>
2024-10-15 09:28:25 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,12 @@ document.addEventListener("DOMContentLoaded", function(){
             }
             else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
@@ -22,9 +24,28 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    }
+    else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    }
+    else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }
+    else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    }
+    else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Uknown game type: ${gameType}. Aborting`
+    }
+
+
 }
 
 
@@ -44,8 +65,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
